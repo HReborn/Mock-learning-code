ae83ffdfe592448cfaa33ba3c9467373ca5f6ffd
Reborn <92050163+HReborn@users.noreply.github.com>
2022-02-13 12:44:06 -0300

    simples classe, com 1 att, get/set, construts, 1 mét e classe main exec ele.

--- App.java
+++ App.java
@@ -1,5 +1,31 @@
 public class App {
+	// Atributo rando
+	private String randoAtt;
+	
+	// seção get set do atributo/s rando
+	public void setRandoAtt (String randoAtt) {
+		this.randoAtt = randoAtt;
+	}
+	public String getRandoAtt () {
+		return this.randoAtt;
+	}
+	
+	// seção construtores do objeto
+	public void App() {
+		this.randoAtt = "Tu eres toba, sabia?";
+	}
+	public void App(String randoAtt) {
+		this.randoAtt = randoAtt;
+	}
+	
+	// seção dos métodos do objeto
+	public void mostrarMsgDentro() {
+		System.out.println(this.randoAtt);
+	}
+	
+	// seção main onde é executada os bagulhos
 	public static void main (String[] args) {
-		System.out.println("Koé, mané");
+		App app = new App();
+		app.mostrarMsgdentro;
 	}
 }
