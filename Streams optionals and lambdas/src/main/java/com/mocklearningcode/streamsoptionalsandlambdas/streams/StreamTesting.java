a2635c40c0aa7e7cc4be328574721d2a5f8d3c58
Reborn <thehitmanreborng@hotmail.com>
2022-09-07 16:16:49 -0300

    testei algumas funções do stream e pŕoximo passo terminar reduce

--- Streams optionals and lambdas/src/main/java/com/mocklearningcode/streamsoptionalsandlambdas/streams/StreamTesting.java
+++ Streams optionals and lambdas/src/main/java/com/mocklearningcode/streamsoptionalsandlambdas/streams/StreamTesting.java
@@ -1,14 +1,37 @@
 package com.mocklearningcode.streamsoptionalsandlambdas.streams;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamTesting {
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("unused")
 		List<Person> ppl = getPeople();
 		
+		Optional<Person> muin = ppl.stream()
+				.filter(p -> p.getAge() < 50)
+				.min(Comparator.comparing((Person::getAge)));
+		System.out.println(muin.orElseThrow());
+		
+		List<Person> newlist = ppl.stream()
+				.filter(p -> p.getAge() < 15)
+				.collect(Collectors.toList());		
+		System.out.println(newlist);
+		
+		String nameFromJoining = ppl.stream()
+				.map(Person::getName)
+				.collect(Collectors.joining("; "));
+		System.out.println(nameFromJoining);
+		
+		Optional<Integer> ageSumFromReduce = ppl.stream()
+				.map(Person::getAge)
+				.reduce((n1, n2) -> n1+n2);
+		System.out.println(ageSumFromReduce.orElseThrow());
 	}
+	
+	
 	private static List<Person> getPeople() {
 	    return List.of(
 	        new Person("Antonio", 20, Gender.MALE),
