4fcd11d46b61b468c17768f2efcc5fed3946cd49
Reborn <thehitmanreborng@hotmail.com>
2022-09-26 18:36:44 -0300

    input error test templates done

--- src/test/java/com/krew/profitcalculator/CalculatorInputErrorHandlingTest.java
+++ src/test/java/com/krew/profitcalculator/CalculatorInputErrorHandlingTest.java
@@ -7,8 +7,24 @@ import org.junit.jupiter.api.Test;
 class CalculatorInputErrorHandlingTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void wrongIslandNameShouldReturnNullAndPrintErrorOnSellRoutesMethod() {
+		assertTrue(true);
+	}
+	@Test
+	void wrongShipNameShouldReturnNullAndPrintErrorOnSellRoutesMethod() {
+		assertTrue(true);
+	}
+	@Test
+	void wrongIslandNameShouldReturnNullAndPrintErrorOnAToBRouteMethod() {
+		assertTrue(true);
+	}
+	@Test
+	void wrongShipNameShouldReturnNullAndPrintErrorOnAToBRouteMethod() {
+		assertTrue(true);
+	}
+	@Test
+	void wrongIslandNameShouldReturnNullAndPrintErrorOnAttackRoutesMethod() {
+		assertTrue(true);
 	}
 
 }
