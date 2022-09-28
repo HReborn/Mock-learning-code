5fc3d68ca54186a7bceda88d95cc9eabf1e28979
Reborn <thehitmanreborng@hotmail.com>
2022-09-28 15:27:49 -0300

    first log message

---
+++ Learning Logging/src/main/java/com/logging/logback/Log.java
@@ -0,0 +1,12 @@
+package com.logging.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log {
+	private static final Logger logger = LoggerFactory.getLogger(Log.class);
+	
+	public static  Logger getLogger() {
+		return logger;
+	}
+}
--- Learning Logging/src/main/java/com/logging/logback/Main.java
+++ Learning Logging/src/main/java/com/logging/logback/Main.java
@@ -1,10 +1,10 @@
 package com.logging.logback;
 
+import org.slf4j.Logger;
+
 public class Main {
-
 	public static void main(String[] args) {
-		
-
+		Logger logger = Log.getLogger();
+		logger.info("First log message from {}", Main.class.getSimpleName());
 	}
-
 }
