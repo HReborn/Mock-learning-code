d46398f7efc2d65c27fb52ed3b15853ae295eb1b
Reborn <thehitmanreborng@hotmail.com>
2022-09-28 21:43:52 -0300

    no need to create a log class. you'll get a logger object from logging factory anyway. and u need to provide the class that's calling the logger.

--- Learning Logging/src/main/java/com/logging/logback/Main.java
+++ Learning Logging/src/main/java/com/logging/logback/Main.java
@@ -1,10 +1,11 @@
 package com.logging.logback;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 	public static void main(String[] args) {
-		Logger logger = Log.getLogger();
+		Logger logger = LoggerFactory.getLogger(Main.class);
 		logger.info("First log message from {}", Main.class.getSimpleName());
 	}
 }
