861d2b2267b67ec955743ff996ebf14b446ad238
Reborn <thehitmanreborng@hotmail.com>
2023-03-06 20:29:03 -0300

    did a file appender and tried new logging methods. Committing this after 3 months away from pc.

--- Learning Logging/src/main/java/com/logging/logback/Main.java
+++ Learning Logging/src/main/java/com/logging/logback/Main.java
@@ -3,9 +3,24 @@ package com.logging.logback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
+
 public class Main {
+
 	public static void main(String[] args) {
 		Logger logger = LoggerFactory.getLogger(Main.class);
-		logger.info("First log message from {}", Main.class.getSimpleName());
+		Logger rootLogger = LoggerFactory.getLogger(Tests.class);
+		rootLogger.atTrace();
+		rootLogger.trace("crappity");
+		Logger root = (Logger)LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+		root.setLevel();
+		
+		try {
+			logger.info("First log message from {}", Main.class.getSimpleName());
+			@SuppressWarnings("unused")
+			int crap = 42/0;
+		} catch (ArithmeticException e) {
+			logger.trace("crazy", e);
+		}
 	}
 }
