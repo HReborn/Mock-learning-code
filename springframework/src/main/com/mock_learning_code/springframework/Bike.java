378fbe2e399da5dbd42de2a30e3ca0af36290d7d
Reborn <thehitmanreborng@hotmail.com>
2023-04-04 16:28:39 -0300

    bean and dependency injection configured with ApplicationContext

---
+++ springframework/src/main/com/mock_learning_code/springframework/Bike.java
@@ -0,0 +1,11 @@
+package com.mock_learning_code.springframework;
+
+public class Bike implements Vehicle {
+
+	@Override
+	public void drive() {
+		System.out.println("Riding a bike.");
+		
+	}
+
+}
