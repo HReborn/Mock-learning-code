be931eeb8267aae5d83c47873f428a988e998182
Reborn <thehitmanreborng@hotmail.com>
2023-04-13 18:34:42 -0300

    spring initializr

---
+++ learningspringboot/src/test/java/com/mock_learning_code/learningspringboot/LearningspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock_learning_code.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
