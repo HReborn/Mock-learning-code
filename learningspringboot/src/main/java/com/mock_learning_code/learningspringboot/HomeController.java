156232652b86403c4de7d0ac36a4e8ffd2c663f7
Reborn <thehitmanreborng@hotmail.com>
2023-04-16 17:35:01 -0300

    now page is accepting inputs through url

--- learningspringboot/src/main/java/com/mock_learning_code/learningspringboot/HomeController.java
+++ learningspringboot/src/main/java/com/mock_learning_code/learningspringboot/HomeController.java
@@ -1,5 +1,7 @@
 package com.mock_learning_code.learningspringboot;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home() {
+	public String home(HttpServletRequest req) {
+		String name = req.getParameter("name");
+		req.setAttribute("name", name);
+//		HttpSession session = req.getSession();
+//		session.setAttribute("name", name);
 		return "home.jsp";
 	}
 
