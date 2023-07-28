df30dca2ed4b8d716266019e2bc0d84f507a044b
Reborn <thehitmanreborng@hotmail.com>
2023-07-27 21:32:27 -0300

    spring initializr

---
+++ src/main/java/com/tenzies/TenziesApplication.java
@@ -0,0 +1,13 @@
+package com.tenzies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenziesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenziesApplication.class, args);
+	}
+
+}
