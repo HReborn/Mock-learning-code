5313f3f13dce46a5aaa0e1e0c0d0265c4ce1c2e0
Reborn <thehitmanreborng@hotmail.com>
2023-10-14 00:38:23 -0300

    bare bones RPC api done

---
+++ firstapi/src/main/java/com/reborn/firstapi/FirstapiApplication.java
@@ -0,0 +1,15 @@
+package com.reborn.firstapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapiApplication.class, args);
+		
+	}
+
+}
