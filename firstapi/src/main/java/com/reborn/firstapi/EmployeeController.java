dc282ba00db72ba5b7a75a91d5186966d04cd874
Reborn <thehitmanreborng@hotmail.com>
2023-11-08 22:01:43 -0300

    HATEOAS representation of findAll GET method

--- firstapi/src/main/java/com/reborn/firstapi/EmployeeController.java
+++ firstapi/src/main/java/com/reborn/firstapi/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.reborn.firstapi;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +24,23 @@ public class EmployeeController {
 		super();
 		this.repository = repository;
 	}
-	
+	/*
 	@GetMapping("/employees")
 	List<Employee> all() {
 		return repository.findAll();
 	}
+	*/
+	@GetMapping("/employees")
+	CollectionModel<EntityModel<Employee>> all() {
+		List<EntityModel<Employee>> employees = repository.findAll().stream()
+				.map(employee -> EntityModel.of(employee,
+						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+						)).collect(Collectors.toList());
+				return CollectionModel.of(employees,
+						linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+	}
+	
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
 		return repository.save(newEmployee);
