bcdb3e2b03b004aac522ea06f58964413f6d3e75
Reborn <92050163+HReborn@users.noreply.github.com>
2025-09-04 01:02:42 +0000

    loop running but with error

--- pythondatascripts/prontuarioDuplicado.py
+++ pythondatascripts/prontuarioDuplicado.py
@@ -2,3 +2,10 @@ import pandas as pd
 path = "data/TiMedReportExport.csv"
 df=pd.read_csv(path, sep=';', usecols=['Alta', 'Nome','Sexo', 'Boletim',])
 print(df.iloc[0,0])
+verificados=0
+for prontAtual in range(19515):
+    for prontTeste in range(0,19515):
+        print(prontTeste)
+        if df.iloc[0,prontAtual] == df.iloc[0,prontTeste]:
+            print("achou um")
+            break
