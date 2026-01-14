259bc46712667d626a29d88b4e95c209b60c1a12
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-14 12:33:21 +0000

    clubs database model created

---
+++ spring-boot/src/main/java/com/mock/spring_boot/models/Club.java
@@ -0,0 +1,36 @@
+package com.mock.spring_boot.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clubs") // Not necessary. Just to be a bit more explicit.
+public class Club {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String photoURL;
+	private String content;
+	@CreationTimestamp // Will initialize the date object and assign it to the entry based on the actual time
+	private LocalDateTime createdOn;
+	@UpdateTimestamp // Will change the date object and assign it to the entry based on the actual time
+	private LocalDateTime updatedOn;
+}
