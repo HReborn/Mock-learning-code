16ff45672a626fe49f7b37323b8ff03c0af62764
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-14 21:03:17 +0000

    Simple files ClubDto and ClubRepository made

---
+++ spring-boot/src/main/java/com/mock/spring_boot/dto/ClubDto.java
@@ -0,0 +1,20 @@
+package com.mock.spring_boot.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+// A DTO tl;dr is when you want to hide certain fields from the client/user. Like a password.
+public class ClubDto {
+	
+	private Long id;
+	private String title;
+	private String photoURL;
+	private String content;
+	private LocalDateTime createdOn;
+	private LocalDateTime updatedOn;
+
+}
