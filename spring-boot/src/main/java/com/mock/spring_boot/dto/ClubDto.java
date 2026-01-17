104eef935398eea0c7f25d35ea0065cb0858b069
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-17 19:05:18 +0000

    simple notempty validation on edit page

--- spring-boot/src/main/java/com/mock/spring_boot/dto/ClubDto.java
+++ spring-boot/src/main/java/com/mock/spring_boot/dto/ClubDto.java
@@ -2,6 +2,7 @@ package com.mock.spring_boot.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,8 +11,11 @@ import lombok.Data;
 public class ClubDto {
 	// A DTO tl;dr is when you want to hide certain fields from the client/user. Like a password.
 	private Long id;
+	@NotEmpty(message = "O título não pode ser vazio, paizão.")
 	private String title;
+	@NotEmpty(message = "A URL não pode ser vazio, paizão.")
 	private String photoURL;
+	@NotEmpty(message = "O conteúdo não pode ser vazio, paizão.")
 	private String content;
 	private LocalDateTime createdOn;
 	private LocalDateTime updatedOn;
