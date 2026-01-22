af9f7acb6e9e7896d313ccfd410d3d943727dac4
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-22 14:23:20 +0000

    bugfix: delete button now working. href link to details from img working.
    changed th:action to th:href. i have no idea when to use one or another. For the details, i didn't do anything. I just started again, clicked on the image and it seemed to redirect just fine now.

--- spring-boot/src/main/java/com/mock/spring_boot/controller/ClubController.java
+++ spring-boot/src/main/java/com/mock/spring_boot/controller/ClubController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -88,12 +87,9 @@ public class ClubController {
 		return "clubs-detail";
 	}
 	
-	@DeleteMapping("/clubs/{clubId}/delete")
-	public String deleteClub (@PathVariable("clubId") Long clubId, 
-							  Model model, 
-							  @ModelAttribute("club") ClubDto club) {
+	@GetMapping("/clubs/{clubId}/delete")
+	public String deleteClub (@PathVariable("clubId") Long clubId) {
 		clubService.deleteClub(clubId);
-		System.out.println("tobaaa");
 		return "redirect:/clubs";
 	}
 }
