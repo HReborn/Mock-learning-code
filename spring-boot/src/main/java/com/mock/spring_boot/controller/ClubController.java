5b3963645a02a8b2d4b3e26cc90028c5648adebd
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-22 07:26:38 +0000

    redirect to detail while clicking done. delete mapping made, but stil doesn't work.

--- spring-boot/src/main/java/com/mock/spring_boot/controller/ClubController.java
+++ spring-boot/src/main/java/com/mock/spring_boot/controller/ClubController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -80,10 +81,19 @@ public class ClubController {
 		return "redirect:/clubs";
 	}
 	
-	@GetMapping("/clubs/{clubId}/")
+	@GetMapping("/clubs/{clubId}")
 	public String getClubs (@PathVariable("clubId") Long clubId, Model model) {
 		ClubDto club = clubService.findById(clubId);
-		model.addAttribute(club);
+		model.addAttribute("club", club);
 		return "clubs-detail";
+	}
+	
+	@DeleteMapping("/clubs/{clubId}/delete")
+	public String deleteClub (@PathVariable("clubId") Long clubId, 
+							  Model model, 
+							  @ModelAttribute("club") ClubDto club) {
+		clubService.deleteClub(clubId);
+		System.out.println("tobaaa");
+		return "redirect:/clubs";
 	}
 }
