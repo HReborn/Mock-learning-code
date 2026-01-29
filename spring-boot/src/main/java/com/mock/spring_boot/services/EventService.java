96c2bc78a03d7da12f1a31cc6de3a5cb57ebc7c7
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-29 08:54:57 +0000

    EventService file created and added create method

--- spring-boot/src/main/java/com/mock/spring_boot/services/EventService.java
+++ spring-boot/src/main/java/com/mock/spring_boot/services/EventService.java
@@ -1,5 +1,9 @@
 package com.mock.spring_boot.services;
 
+import com.mock.spring_boot.dto.EventDto;
+
 public interface EventService {
+	
+	void createEvent(Long clubId, EventDto eventDto);
 
 }
--- spring-boot/src/main/java/com/mock/spring_boot/services/impl/EventServiceImpl.java
+++ spring-boot/src/main/java/com/mock/spring_boot/services/impl/EventServiceImpl.java
@@ -1,7 +1,45 @@
 package com.mock.spring_boot.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mock.spring_boot.dto.EventDto;
+import com.mock.spring_boot.models.Club;
+import com.mock.spring_boot.models.Event;
+import com.mock.spring_boot.repositories.ClubRepository;
+import com.mock.spring_boot.repositories.EventRepository;
 import com.mock.spring_boot.services.EventService;
 
 public class EventServiceImpl implements EventService {
+
+	private EventRepository eventRepository;	
+	private ClubRepository clubRepository;
+
+	@Autowired
+	public EventServiceImpl(EventRepository eventRepository, ClubRepository clubRepository) {
+		super();
+		this.eventRepository = eventRepository;
+		this.clubRepository = clubRepository;
+	}
+
+	@Override
+	public void createEvent(Long clubId, EventDto eventDto) {
+		Club club = clubRepository.findById(clubId).get();
+		Event event = mapToEvent(eventDto);
+		event.setClub(club);
+		eventRepository.save(event);
+	}
+	
+	private Event mapToEvent(EventDto eventDto) {
+		return Event.builder()
+				.id(eventDto.getId())
+				.name(eventDto.getName())
+				.startTime(eventDto.getStartTime())
+				.endTime(eventDto.getEndTime())
+				.type(eventDto.getType())
+				.photoURL(eventDto.getPhotoURL())
+				.createdOn(eventDto.getCreatedOn())
+				.updatedOn(eventDto.getUpdatedOn())
+				.build();
+	}
 
 }
