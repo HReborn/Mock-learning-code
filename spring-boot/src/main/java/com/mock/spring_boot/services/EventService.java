372e09bd79e1ee07dd2093f342f9a40e3cbc361d
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-29 17:03:40 +0000

    Added save method on service and solved Bean bug
    Forgot about the @Service annotation on the EventServiceImpl file to indicate that it's a bean and autoinitialize

--- spring-boot/src/main/java/com/mock/spring_boot/services/EventService.java
+++ spring-boot/src/main/java/com/mock/spring_boot/services/EventService.java
@@ -1,9 +1,11 @@
 package com.mock.spring_boot.services;
 
 import com.mock.spring_boot.dto.EventDto;
+import com.mock.spring_boot.models.Event;
 
 public interface EventService {
 	
 	void createEvent(Long clubId, EventDto eventDto);
+	Event saveEvent(EventDto eventDto);
 
 }
