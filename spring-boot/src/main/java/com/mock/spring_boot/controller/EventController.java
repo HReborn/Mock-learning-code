e61671fdb67b0d6b2e64102880092e15de932304
Reborn <92050163+HReborn@users.noreply.github.com>
2026-01-29 09:15:58 +0000

    get mapping on create event implemented

--- spring-boot/src/main/java/com/mock/spring_boot/controller/EventController.java
+++ spring-boot/src/main/java/com/mock/spring_boot/controller/EventController.java
@@ -2,7 +2,11 @@ package com.mock.spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.mock.spring_boot.models.Event;
 import com.mock.spring_boot.services.EventService;
 
 @Controller
@@ -14,5 +18,13 @@ public class EventController {
 	public EventController(EventService eventService) {
 		super();
 		this.eventService = eventService;
+	}
+	
+	@GetMapping("/clubs/{clubId}/new")
+	public String createEvent(@PathVariable("clubId") Long clubId, Model model ) {
+		Event event = new Event();
+		model.addAttribute("clubId", clubId);
+		model.addAttribute("event", event);
+		return "events-create";
 	}
 }
