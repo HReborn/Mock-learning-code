26f3336b0943ba00af24434228fbd595597f6c19
Reborn <92050163+HReborn@users.noreply.github.com>
2026-02-15 22:20:52 +0000

    register controller created. linked with html.

--- spring-boot/src/main/java/com/mock/spring_boot/repositories/UserRepository.java
+++ spring-boot/src/main/java/com/mock/spring_boot/repositories/UserRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mock.spring_boot.models.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+	
+	UserEntity findByEmail(String email);
+	UserEntity findByUsername(String username);
 
 }
--- spring-boot/src/main/java/com/mock/spring_boot/services/UserService.java
+++ spring-boot/src/main/java/com/mock/spring_boot/services/UserService.java
@@ -1,7 +1,11 @@
 package com.mock.spring_boot.services;
 
 import com.mock.spring_boot.dto.RegistrationDto;
+import com.mock.spring_boot.models.UserEntity;
 
 public interface UserService {
 	void registerUser(RegistrationDto registrationDto);
+
+	UserEntity findByEmail(String email);
+	UserEntity findByUsername(String username);
 }
