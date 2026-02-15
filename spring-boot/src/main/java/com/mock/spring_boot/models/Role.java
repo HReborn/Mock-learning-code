4d4431d647e9281dbd6f3827941b6bdf2af7d47b
Reborn <92050163+HReborn@users.noreply.github.com>
2026-02-15 19:40:10 +0000

    roles model created

--- spring-boot/src/main/java/com/mock/spring_boot/models/Role.java
+++ spring-boot/src/main/java/com/mock/spring_boot/models/Role.java
@@ -1,5 +1,24 @@
 package com.mock.spring_boot.models;
 
-public class Role {
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@ManyToMany(mappedBy = "roles")
+	private List<UserEntity> users = new ArrayList<>();
 }
