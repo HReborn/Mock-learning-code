d79b38368d7f3d9c054a8df46af7360bb26f3bce
Reborn <92050163+HReborn@users.noreply.github.com>
2026-02-16 13:45:52 +0000

    refactored the user check if exists

--- spring-boot/src/main/java/com/mock/spring_boot/controller/AuthController.java
+++ spring-boot/src/main/java/com/mock/spring_boot/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mock.spring_boot.dto.RegistrationDto;
-import com.mock.spring_boot.models.UserEntity;
 import com.mock.spring_boot.services.UserService;
 
 import jakarta.validation.Valid;
@@ -39,11 +38,9 @@ public class AuthController {
 								@Valid @ModelAttribute("user") RegistrationDto registrationDto, 
 								BindingResult result,
 								RedirectAttributes redirectAttributes) {
-		UserEntity existingEmail = userService.findByEmail(registrationDto.getEmail());
-		UserEntity existingUsername = userService.findByUsername(registrationDto.getUsername());
-		if (existingEmail != null || existingUsername != null) {
-			result.rejectValue("username", "error.user", "An account already exists for this email/username.");
-			result.rejectValue("email", "error.user", "An account already exists for this email/username.");
+		boolean fieldsNotEmpty = !result.hasFieldErrors();
+		if (fieldsNotEmpty) {
+			checkUserUniqueness(registrationDto, result);
 		}
 		if (result.hasErrors()) {
 			// These two lines link the error from result to the registrationDto and make it available in the redirect.
@@ -56,6 +53,21 @@ public class AuthController {
 		// the ?success is a query parameter and will link to the view with th:if="${param.success}" to show a success message
 		// on the clubs page. With this return, the URL becomes http://localhost:8080/clubs?success
 		return "redirect:/clubs?success";
+	}
+	
+	// I opted to extract the method to avoid a floating variable and to avoid nesting
+	// because emailExists might still be false because we never checked DB
+	// because the check would only happen if the fields are not empty, but the email and username might still be registered.
+	// The meaning of the variable emailExists is ambiguous because it could be false if the email isn't registered but also if we never checked DB because the validation failed.
+	// Single responsibility of Variables: A variable should represent one clear, unambiguous state.
+	private void checkUserUniqueness(RegistrationDto registrationDto, BindingResult result) {
+		boolean emailRegistered = userService.findByEmail(registrationDto.getEmail()) != null;
+		boolean userRegistered = userService.findByUsername(registrationDto.getUsername()) != null;
+		if (emailRegistered || userRegistered) {
+			String errorMessage = "An account already exists for this email/username.";
+			result.rejectValue("username", "error.user", errorMessage);
+			result.rejectValue("email", "error.user", errorMessage);
+		}
 	}
 	
 	@GetMapping("/login")
