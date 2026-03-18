11089ee65276074c2a2265e75080aae107963b26
Reborn <92050163+HReborn@users.noreply.github.com>
2026-03-18 21:57:02 +0000

    BUGFIX: Role model didn't have default constructor.

--- spring-boot/src/main/java/com/mock/spring_boot/models/Role.java
+++ spring-boot/src/main/java/com/mock/spring_boot/models/Role.java
@@ -8,14 +8,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity(name = "roles")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
