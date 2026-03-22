8e8136a4aef962cdc04de66a00d7c4dc62d0781f
Reborn <92050163+HReborn@users.noreply.github.com>
2026-03-22 20:55:53 +0000

    BUGFIX: Redirects now show messages

--- spring-boot/src/main/java/com/mock/spring_boot/GlobalExceptionHandler.java
+++ spring-boot/src/main/java/com/mock/spring_boot/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package com.mock.spring_boot;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.servlet.http.HttpSession;
 
 // this annotation will act as a global try-catch for the @Controller annotation
 // It'll handle all exceptions
@@ -15,9 +14,18 @@ import jakarta.servlet.http.HttpSession;
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ResponseStatusException.class)
-	public String handleNotFound(HttpSession session, ResponseStatusException ex, Model model) {
+	public String handleNotFound(ResponseStatusException ex, 
+								 RedirectAttributes redirectAttributes) {
+		
 		if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
-			session.setAttribute("errorMessage",  "The page you are trying to access, does not exist.");
+			// Using this is Spring's way of adding an attribute that survives a redirect.
+			// Stores it on a temporary flash map. But for it to work you NEED to use "	redirect:".
+			// It's lifetime is only one redirect, only one next request.
+			
+			redirectAttributes.addFlashAttribute(
+		            "errorMessage",
+		            "The page you are trying to access does not exist."
+		        );
 			return "redirect:/";
 		}
 		throw ex; // Let other status codes pass
